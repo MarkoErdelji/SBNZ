fee1f9f8463bfcb3d3a3aa4d6a7101aa3774566d
MarkoErdelji <erdeljimarko@gmail.com>
2024-06-05 18:31:41 +0200

    feat: Added templates for base rules

--- Kostur/IME_PREZIME_BROJINDEKSA_GRUPA/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/config/AppInitializer.java
+++ Kostur/IME_PREZIME_BROJINDEKSA_GRUPA/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/config/AppInitializer.java
@@ -1,28 +1,36 @@
 package com.ftn.sbnz.config;
 
-
 import com.ftn.sbnz.model.User;
 import com.ftn.sbnz.repository.UserRepository;
+import org.drools.core.io.impl.ClassPathResource;
+import org.drools.template.DataProvider;
+import org.drools.template.DataProviderCompiler;
+import org.drools.template.objects.ArrayDataProvider;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.drools.decisiontable.ExternalSpreadsheetCompiler;
 
+import java.io.*;
 import java.util.List;
 
 @Configuration
 public class AppInitializer {
+
     @Autowired
     KieSession kieSession;
+
     @Bean
     public ApplicationRunner applicationRunner(UserRepository userRepository) {
         return args -> {
-            // This code will run after the application context is loaded
             List<User> users = userRepository.findAll();
             users.forEach(user -> {
                 kieSession.insert(user);
             });
+
         };
     }
 }
